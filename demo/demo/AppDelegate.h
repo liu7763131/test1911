9ef03c26afe0d493912ba4fc84fc4d564e34766e
liu7763131 <liu7763131@sina.cn>
2016-01-26 18:04:15 +0800

    adad

---
+++ demo/demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by qianfeng on 16/1/26.
+//  Copyright © 2016年 刘博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
